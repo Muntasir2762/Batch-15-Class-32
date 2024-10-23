70cd5af7be2b838caf8ddc729b26a707ae629ce8
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-10-23 23:52:51 +0600

    Js nested loop and function intro

---
+++ script.js
@@ -0,0 +1,60 @@
+
+let marks = [
+    [34, 65, 89, 90, 32],
+    [56, 78, 88, 67, 99],
+    [43, 67, 89, 78,9],
+];
+
+//rowXcolumn => 3X5 = 15
+
+// console.log(marks[0][0]);
+// console.log(marks[0][1]);
+// console.log(marks[0][2]);
+// console.log(marks[0][3]);
+// console.log(marks[0][4]);
+
+// console.log(marks[1][0]);
+// console.log(marks[1][1]);
+// console.log(marks[1][2]);
+// console.log(marks[1][3]);
+// console.log(marks[1][4]);
+
+// console.log(marks[2][0]);
+// console.log(marks[2][1]);
+// console.log(marks[2][2]);
+// console.log(marks[2][3]);
+// console.log(marks[2][4]);
+
+// for (i = 0; i<=2; i++){ //i = 0, 1, 2
+//     if(i == 0){
+//         console.log("SectionA");
+//     }
+//     else if(i == 1){
+//         console.log("SectionB");
+//     }
+//     else if(i == 2){
+//         console.log("SectionC");
+//     }
+//     for(j = 0; j<=4; j++){ //j = 0, 1, 2, 3, 4 | j = 0, 1, 2, 3, 4 | j = 0, 1, 2, 3, 4
+//         // console.log(marks[i][j]);
+
+//         if(marks[i][j] >= 33){
+//             console.log(marks[i][j] + "=Pass");
+//         }
+
+//         else{
+//             console.log(marks[i][j] + "=Fail");
+//         }
+//     }
+// }
+
+function sum ()
+{
+    let x = 10;
+    let y = 20;
+
+    let result = x+y;
+    console.log(result); //30
+}
+
+sum();
